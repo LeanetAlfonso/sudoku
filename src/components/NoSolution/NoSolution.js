a60bdab97f8ed93f87c036aa168023d0a46f7405
LeanetAlfonso <leanet.alfonso1@gmail.com>
2022-12-03 15:55:04 -0500

    Update and test  No Solution modal

--- src/components/NoSolution/NoSolution.js
+++ src/components/NoSolution/NoSolution.js
@@ -5,26 +5,45 @@ import Dialog from '@mui/material/Dialog';
 import Typography from '@mui/material/Typography';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-
+import IconButton from '@mui/material/IconButton';
+import { useTranslation } from "react-i18next";
+import './NoSolution.css';
 export default function NoSolution(props) {
 
     const { noSolution, setNoSolution } = props;
-    return (
-        <Dialog open={noSolution.isOpen} data-testid="no-solution">
-            <DialogTitle variant="h3">
-                No Solution Found
+    const { t } = useTranslation();
+    const storedTheme = localStorage.getItem("theme");
+
+    return (noSolution.isOpen &&
+        <Dialog
+            data-testid="no-solution"
+            open={noSolution.isOpen}
+            // dark mode support
+            PaperProps={{
+                style: {
+                    backgroundColor: storedTheme === "dark" ? "#242727" : "#eee",
+                    color: storedTheme === "dark" ? "#dbd7d7" : "#333",
+                    boxShadow: 'none',
+                },
+            }}>
+            <IconButton disableRipple className='details-icon'>
+                <i className={`fa-solid fas fa-exclamation custom-error fa-4x`}></i>
+            </IconButton>
+            <DialogTitle variant="h5">
+                {t('no_solution_title')}
             </DialogTitle>
             <DialogContent>
                 <Typography variant="subtitle1">
-                    This grid cannot be solved.
+                    {t('no_solution_first_subtitle')}
                 </Typography>
                 <Typography variant="subtitle1">
-                    Please start a new game.
+                    {t('no_solution_second_subtitle')}
                 </Typography>
             </DialogContent>
             <DialogActions>
                 <Button
-                    text="OK"
+                    testId='ok-no-solution'
+                    text={t('ok')}
                     onClick={() => setNoSolution({ ...noSolution, isOpen: false })}
                     buttonStyle="btn--primary--solid"
                 />
--- src/components/NoSolution/__test__/NoSolution.test.js
+++ src/components/NoSolution/__test__/NoSolution.test.js
@@ -1,6 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import NoSolution from '../NoSolution';
 
+jest.mock('react-i18next', () => ({
+    // this mock makes sure any components using the translate hook can use it without a warning being shown
+    useTranslation: () => {
+        return {
+            t: (str) => str,
+            i18n: {
+                changeLanguage: () => new Promise(() => { /* mock changeLanguage */ }),
+            },
+        };
+    },
+}));
+
 describe('NoSolution', () => {
     it('should render the no solution dialog', () => {
         render(<NoSolution noSolution={{ isOpen: true }} />);
